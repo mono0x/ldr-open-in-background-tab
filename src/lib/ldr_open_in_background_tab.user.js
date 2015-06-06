9ed493846647adf781595bd0f53a91b81a239d2d
mono <mono@monoweb.info>
2015-06-07 02:05:18 +0900

    Update LICENSE

--- src/lib/ldr_open_in_background_tab.user.js
+++ src/lib/ldr_open_in_background_tab.user.js
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014 os0x
- * Copyright (c) 2014 mono
+ * Copyright (c) 2014-2015 mono
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
